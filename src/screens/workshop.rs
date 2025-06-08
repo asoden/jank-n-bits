9fa4c29003011f3364231ca63f94c9b299ffc366
Andrew Soden <soden.andrew@gmail.com>
2025-06-08 01:04:11 -0500

    Add a rudimentary UI node for workspace

--- src/screens/workshop.rs
+++ src/screens/workshop.rs
@@ -1,8 +1,17 @@
 //! Screen for creating firework bits.
 
-use bevy::{input::common_conditions::input_just_pressed, prelude::*, ui::Val::*};
+use bevy::{
+    image::{ImageLoaderSettings, ImageSampler},
+    input::common_conditions::input_just_pressed,
+    prelude::*,
+    ui::Val::*,
+};
 
 use crate::{Pause, menus::Menu, screens::Screen, theme::widget};
+
+const WORKSHOP_TILE_WIDTH: f32 = 64.;
+const WORKSHOP_COLUMNS: f32 = 8.;
+const WORKSHOP_ROWS: f32 = 8.;
 
 pub(super) fn plugin(app: &mut App) {
     app.add_systems(OnEnter(Screen::Workshop), spawn_workshop);
@@ -30,11 +39,70 @@ pub(super) fn plugin(app: &mut App) {
     );
 }
 
-fn spawn_workshop(mut commands: Commands) {
+fn spawn_workshop(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let workspace_sidebar = asset_server.load("images/workspace-panel.png");
+    let workspace_texture = asset_server.load_with_settings(
+        "images/workspace.png",
+        |settings: &mut ImageLoaderSettings| {
+            // Need to use nearest filtering to avoid bleeding between the slices with tiling
+            settings.sampler = ImageSampler::nearest();
+        },
+    );
+
+    let slicer = TextureSlicer {
+        border: BorderRect::all(WORKSHOP_TILE_WIDTH),
+        center_scale_mode: SliceScaleMode::Tile { stretch_value: 1. },
+        sides_scale_mode: SliceScaleMode::Tile { stretch_value: 1. },
+        ..default()
+    };
+
+    commands
+        .spawn((
+            Node {
+                width: Val::Percent(100.),
+                height: Val::Percent(100.),
+                justify_content: JustifyContent::Center,
+                align_content: AlignContent::Center,
+                flex_wrap: FlexWrap::Wrap,
+                column_gap: Val::Px(0.),
+                row_gap: Val::Px(0.),
+                ..default()
+            },
+            StateScoped(Screen::Workshop),
+            Pickable::IGNORE,
+        ))
+        .with_children(|parent| {
+            parent.spawn((
+                ImageNode {
+                    image: workspace_sidebar.clone(),
+                    image_mode: NodeImageMode::Sliced(slicer.clone()),
+                    ..default()
+                },
+                Node {
+                    width: Val::Px(128.),
+                    height: Val::Px(64. * WORKSHOP_ROWS),
+                    ..default()
+                },
+                StateScoped(Screen::Workshop),
+            ));
+            parent.spawn((
+                ImageNode {
+                    image: workspace_texture.clone(),
+                    image_mode: NodeImageMode::Sliced(slicer.clone()),
+                    ..default()
+                },
+                Node {
+                    width: Val::Px(64. * WORKSHOP_COLUMNS),
+                    height: Val::Px(64. * WORKSHOP_ROWS),
+                    ..default()
+                },
+                StateScoped(Screen::Workshop),
+            ));
+        });
+
     commands.spawn((
-        widget::ui_root("Workshop Menu"),
+        widget::button("Launch bits!", launch_bits),
         StateScoped(Screen::Workshop),
-        children![widget::button("Launch bits!", launch_bits),],
     ));
 }
 
